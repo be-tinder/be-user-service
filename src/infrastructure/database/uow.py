9c93abb0b0235e54a87455c5f5a9da669fa065a4
dimash <arystambekdimash005@gmail.com>
2025-03-11 10:50:06 +0500

    upd: in __init__ removed super extension

--- src/infrastructure/database/uow.py
+++ src/infrastructure/database/uow.py
@@ -5,7 +5,6 @@ from src.domain.interfaces.uow import IUoW
 
 class UoW(IUoW):
     def __init__(self, session: AsyncSession):
-        super().__init__()
         self._session = session
 
     async def commit(self):
